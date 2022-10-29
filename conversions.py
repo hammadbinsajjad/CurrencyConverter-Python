01ffa969b23b77846e67b2ca0da247fd01c8cc16
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2022-10-29 18:42:56 +0500

    Merged all conversion into one file

--- conversions.py
+++ conversions.py
@@ -6,3 +6,63 @@ def sar_to_pkr(amount):
 
 def pkr_to_pound(amount):
     return 0.0039 * amount
+
+def pkr_to_aed(amount):
+    return 0.017 * amount
+
+def pkr_to_russianruble(amount):
+    return amount*0.28
+
+def pkr_to_yuan(amount):
+    return 0.033 * amount
+
+def yuan_to_pkr(y_amount):
+    return y_amount*30.46
+
+def pkr_to_bahrain_dinar(amount):
+    return 0.0017 * amount
+
+def bahrain_dinar_to_pkr(b_amount):
+    return b_amount*586.18
+
+def pkr_to_inr(amount):
+    return 0.37 * amount
+
+def inr_to_pkr(i_amount):
+    return i_amount*2.69
+
+def takka_to_pkr(t_amount):
+    return 2.18 * t_amount
+
+def pkr_to_takka(amount):
+    return amount*0.46
+
+def dkk_to_pkr(d_amount):
+    return 29.57 * d_amount
+
+def pkr_to_dkk(amount):
+    return amount*0.034
+
+def euro_to_pkr(amount):
+    return 0.0045 * amount
+
+def pkr_to_euro(amount):
+    return amount*220.61
+
+def won_to_pkr(amount):
+    return 0.16 * amount
+
+def pkr_to_won(amount):
+    return amount*6.42
+
+def lira_to_pkr(amount):
+    return 11.91 * amount
+
+def pkr_to_lira(amount):
+    return amount*0.084
+
+def pkr_to_rial(amount):
+    return 0.0052* amount
+
+def rial_to_pkr(amount):
+    return amount*191.53
--- main.py
+++ main.py
@@ -5,9 +5,30 @@ def main():
         print("Here are the choices for converions")
 
         # Enter your conversions here
-        print("1. PKR to USD")
-        print("2. SAR to PKR")
-        print("3. PKR to Pound")
+        print(" 1. PKR to USD")
+        print(" 2. SAR to PKR")
+        print(" 3. PKR to GBP")
+        print(" 4. PKR to AED")
+        print(" 5. PKR to RUR")
+        print(" 6. PKR to CNY")
+        print(" 7. CNY to PKR")
+        print(" 8. PKR to BHD")
+        print(" 9. BHD to PKR")
+        print("10. PKR to INR")
+        print("11. INR to PKR")
+        print("12. BDT to PKR")
+        print("13. PKR to BDT")
+        print("14. DKK to PKR")
+        print("15. PKR to DKK")
+        print("16. EUR to PKR")
+        print("17. PKR to EUR")
+        print("18. WON to PKR")
+        print("19. PKR to WON")
+        print("20. LIRA to PKR")
+        print("21. PKR to LIRA")
+        print("22. PKR to RIAL")
+        print("23. RIAL to PKR")
+
 
         choice = int(input("Enter your conversion choice: "))
 
@@ -24,6 +45,86 @@ def main():
             
         if choice == 3:
             converted_amount = pkr_to_pound(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 4:
+            converted_amount = pkr_to_aed(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 5:
+            converted_amount = pkr_to_russianruble(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 6:
+            converted_amount = pkr_to_yuan(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 7:
+            converted_amount = yuan_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 8:
+            converted_amount = pkr_to_bahrain_dinar(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 9:
+            converted_amount = bahrain_dinar_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 10:
+            converted_amount = pkr_to_inr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 11:
+            converted_amount = inr_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 12:
+            converted_amount = takka_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 13:
+            converted_amount = pkr_to_takka(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 14:
+            converted_amount = dkk_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 15:
+            converted_amount = pkr_to_dkk(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 16:
+            converted_amount = euro_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 17:
+            converted_amount = pkr_to_euro(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 18:
+            converted_amount = won_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 19:
+            converted_amount = pkr_to_won(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 20:
+            converted_amount = lira_to_pkr(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 21:
+            converted_amount = pkr_to_lira(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 22:
+            converted_amount = pkr_to_rial(amount)
+            print(f"The converted amount is {converted_amount}")
+
+        if choice == 23:
+            converted_amount = rial_to_pkr(amount)
             print(f"The converted amount is {converted_amount}")
 
         cont = input("Do you want to continue? (Y)es or (N)o: ")
